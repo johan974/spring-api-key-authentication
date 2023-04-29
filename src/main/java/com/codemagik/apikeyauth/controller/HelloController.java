ad51ce42a5513575aa25145c8d045f547b2271c3
johan974 <jj974@upcmail.nl>
2023-04-29 20:51:41 +0200

    Added: CORS (but does not work yet)

--- src/main/java/com/codemagik/apikeyauth/controller/HelloController.java
+++ src/main/java/com/codemagik/apikeyauth/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.codemagik.apikeyauth.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,10 @@ public class HelloController {
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
         return ResponseEntity.status(HttpStatus.OK).body("hello");
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<String> doPost() {
+        return ResponseEntity.status(HttpStatus.OK).body("post hello");
     }
 }
--- src/main/java/com/codemagik/apikeyauth/security/SecurityConfiguration.java
+++ src/main/java/com/codemagik/apikeyauth/security/SecurityConfiguration.java
@@ -8,10 +8,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
+@EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
@@ -28,9 +35,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.authenticationProvider(getAPIAuthenticationProvider());
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://example.com")); // cors.setAllowedOrigins(List.of("http://localhost:4200",
+        configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "OPTIONS"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(List.of("AuthorizationX", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/api/isAlive").permitAll()
                 .anyRequest().authenticated()
                 .and()
